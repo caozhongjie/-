ce83e9fc3fab2ea7c14a29f7ea0a131f09ef577d
caozhongjie <30990994+caozhongjie@users.noreply.github.com>
2018-08-24 17:02:24 +0800

    submit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <header></header>
+    <div class="tab">
+    i am table
+    </div>
+    <div class="content">
+    i am content
+    </div>
+
+
+
+  </div>
+</template>
+
+<script>
+  import header from 'components/header/header.vue'
+export default {
+ components:{
+   header
+ }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
